2502df6ec9d14ca911a8c09b589b263f758fb4a3
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-01-27 17:02:34 +0100

    Add window styles before the first frame is rendered

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,12 @@
-use crate::epi::egui::CtxRef;
-use crate::epi::Frame;
+use crate::epi::egui::{CtxRef, Rgba};
+use crate::epi::{Frame, Storage};
 use clap::Parser;
-use eframe::{epi, NativeOptions};
+use eframe::epi;
+use eframe::NativeOptions;
 use windows::Win32::Foundation::HWND;
+use windows::Win32::UI::Input::KeyboardAndMouse;
+use windows::Win32::UI::WindowsAndMessaging;
+use windows::Win32::UI::WindowsAndMessaging::{GWL_EXSTYLE, WS_EX_NOACTIVATE, WS_EX_TRANSPARENT};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
@@ -47,7 +51,24 @@ impl App {
 impl epi::App for App {
     fn update(&mut self, _ctx: &CtxRef, _frame: &Frame) {}
 
+    fn setup(&mut self, _ctx: &CtxRef, _frame: &Frame, _storage: Option<&dyn Storage>) {
+        let overlay = unsafe { KeyboardAndMouse::GetActiveWindow() };
+        self.overlay = Some(overlay);
+
+        unsafe {
+            WindowsAndMessaging::SetWindowLongPtrA(
+                overlay,
+                GWL_EXSTYLE,
+                (WS_EX_TRANSPARENT | WS_EX_NOACTIVATE) as isize,
+            );
+        }
+    }
+
     fn name(&self) -> &str {
         "Example App"
+    }
+
+    fn clear_color(&self) -> Rgba {
+        Rgba::TRANSPARENT
     }
 }
