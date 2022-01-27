080af478a60323dd187ee3d1847b16a3be15facf
Yannic Nuwenhof <ynuwenhof@pm.me>
2022-01-27 17:20:39 +0100

    Update repainting and scale the window according to the target

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,15 @@
-use crate::epi::egui::{CtxRef, Rgba};
-use crate::epi::{Frame, Storage};
 use clap::Parser;
+use eframe::egui::containers;
+use eframe::egui::{Color32, CtxRef, Rgba, Window};
 use eframe::epi;
+use eframe::epi::{Frame, Storage};
 use eframe::NativeOptions;
-use windows::Win32::Foundation::HWND;
+use windows::Win32::Foundation::{HWND, RECT};
 use windows::Win32::UI::Input::KeyboardAndMouse;
 use windows::Win32::UI::WindowsAndMessaging;
-use windows::Win32::UI::WindowsAndMessaging::{GWL_EXSTYLE, WS_EX_NOACTIVATE, WS_EX_TRANSPARENT};
+use windows::Win32::UI::WindowsAndMessaging::{
+    GWL_EXSTYLE, HWND_TOPMOST, WS_EX_NOACTIVATE, WS_EX_TRANSPARENT,
+};
 
 #[derive(Parser, Debug)]
 #[clap(author, version, about)]
@@ -49,7 +52,56 @@ impl App {
 }
 
 impl epi::App for App {
-    fn update(&mut self, _ctx: &CtxRef, _frame: &Frame) {}
+    fn update(&mut self, ctx: &CtxRef, _frame: &Frame) {
+        let overlay = match self.overlay {
+            None => return,
+            Some(overlay) => overlay,
+        };
+
+        let mut rect = RECT::default();
+        unsafe {
+            WindowsAndMessaging::GetWindowRect(self.target, &mut rect);
+        }
+
+        unsafe {
+            WindowsAndMessaging::SetWindowPos(
+                overlay,
+                HWND_TOPMOST,
+                rect.left,
+                rect.top,
+                rect.right - rect.left,
+                rect.bottom - rect.top,
+                0,
+            );
+        }
+
+        Window::new(self.name())
+            .frame(containers::Frame::window(&ctx.style()).shadow(Default::default()))
+            .show(ctx, |ui| {
+                ui.horizontal_wrapped(|ui| {
+                    ui.label("Overlay handle: ");
+
+                    ui.colored_label(
+                        Color32::from_rgb(110, 255, 110),
+                        format!("{:#X}", overlay.0),
+                    );
+                });
+
+                ui.horizontal_wrapped(|ui| {
+                    ui.label("Target handle: ");
+
+                    ui.colored_label(
+                        Color32::from_rgb(110, 255, 110),
+                        format!("{:#X}", self.target.0),
+                    );
+                });
+
+                ui.separator();
+                ui.vertical_centered(|ui| {
+                    ui.hyperlink("https://github.com/ynuwenhof/egui-overlay")
+                });
+            });
+    }
 
     fn setup(&mut self, _ctx: &CtxRef, _frame: &Frame, _storage: Option<&dyn Storage>) {
         let overlay = unsafe { KeyboardAndMouse::GetActiveWindow() };
